f199d0982fdfe856eb2ef9a385b59ec9a7bd21b1
PDrocco <pedroicostapc@gmail.com>
2025-08-18 18:29:31 -0300

    atualização

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Crochet da Mimi',
+  description: 'Loja online de crochês artesanais. Encontre bolsas, amigurumis e itens de decoração feitos com muito afeto.',
 }
 
 export default function RootLayout({
